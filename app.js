b9a73c037ca855056642e001dfdcf70235fbfc5b
Shivank12batra <Shivank56batra@gmail.com>
2023-01-21 02:29:28 +0530

    database setup and user model setup done

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(errorHandlerMiddleware);
 
 const start = async() => {
     try {
-        // await connectDB(process.env.MONGO_URI)
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening at ${port}`))
     } catch(err) {
         console.log(err)
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        maxlength: 50,
+        minlength: 3
+    },
+    phoneNumber: {
+        type: Number,
+        required: [true, 'Please provide contact number'],
+        maxlength: 10
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+          ],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 4
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
