b5f72d212d97a4ddea5f34051131a44a401a8861
Shivank12batra <Shivank56batra@gmail.com>
2023-01-24 02:31:13 +0530

    register route bug fixed

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,13 +1,16 @@
 const User = require('../models/User');
 const checkCredentials = require('../scraper/login');
-const UnauthenticatedError = require('../errors');
+const {UnauthenticatedError} = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const register = async(req, res) => {
+    const userPassword = req.body.password;
     const user = await User.create({...req.body})
-    const isValid = await checkCredentials(user.email, user.password).then(response => response)
+    console.log(user)
+    const isValid = await checkCredentials(user.email, userPassword).then(response => response)
+    console.log(isValid)
     if (!isValid) {
-        await User.findOneAndDelete(user)
+        await User.findOneAndDelete({email : user.email})
         throw new UnauthenticatedError('Invalid Credentials!')
     }
     res.status(StatusCodes.CREATED).json({msg: `You have been subscribed to the attendance chatbot and will be receiving updates on ${user.contactNumber}`})
